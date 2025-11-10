c5bce0147f9d89975105b9b9d9f3b930f2401c87
Amina842910 <amina842910@gmail.com>
2025-11-10 21:50:03 +0600

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -80,7 +80,7 @@ const Contact = () => {
                       <label htmlFor="email" className="block text-sm font-medium text-gray-700">
                         Email address
                       </label>
-                      //<input type="text" name="email" id="email" className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md p-2 border" />
+                      /* <input type="text" name="email" id="email" className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md p-2 border" /> */
                         <input type="hidden" name="_subject" value="New Contact Message" />
                     </div>
                     <div className="col-span-6 sm:col-span-3">
